5d87cc364bbf520d384446bd0771e49b848fbdeb
Rodrigo Perez Domanico <rodrigoperez33@hotmail.com>
2023-09-02 04:31:57 -0300

    Fetching data from Rick and Morty API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+'use client';
+
 import React from 'react'
 import FormSearchUser from '@/components/FormSearchUser'
+// import { useState } from 'react';
+// import { User } from "@/interfaces/user";
 
 const Home = () => {
+  // const [user, setUser] = useState<User | null>(null);
+  // const [error, setError] = useState<string | null>(null);
+  const getUser = async (username:number)=>{
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${username}`);
+    const data = res.json()
+    if(!res.ok){
+      console.log("User not found")
+      // setUser(null);
+      // setError("User not found");
+      return ;
+
+    }
+
+    console.log(data);
+    
+  }
+  
   return(
-  <FormSearchUser/>
+    <>
+  <FormSearchUser getUser={getUser}/>
+  
+  </>
   )
 }
 
--- src/components/FormSearchUser.tsx
+++ src/components/FormSearchUser.tsx
@@ -1,15 +1,27 @@
 import SearchIcon from "@/components/icons/SearchIcon";
+import React from "react";
+
+interface Props {
+  getUser: (username:number)=>Promise<void>;
+}
 
 
-const FormSearchUser = ()=>{
-    
+
+const FormSearchUser = ({ getUser }: Props)=>{
+    const handleType = async(e:React.FormEvent<HTMLFormElement>)=>{
+      e.preventDefault();
+      const username= e.currentTarget.username.value
+      if(!username)return;
+      await getUser(username)
+      console.log(username)
+    }
     return (
         <div className='flex justify-center w-screen'>
-          <form className="grid gap-2 w-screen grid-cols-[1fr_10fr_3fr] bg-blue-900 mx-5 py-2 rounded-md max-w-3xl ">
+          <form onChange={handleType} className="grid gap-2 w-screen grid-cols-[1fr_10fr_3fr] bg-blue-900 mx-5 py-2 rounded-md max-w-3xl ">
             <span>
               <SearchIcon className='fill-blue-300' />
             </span>
-            <input  type='text' placeholder='Type the username' className='bg-transparent h-full outline-none'/>
+            <input name="username" type='text' placeholder='Type the username' className='bg-transparent h-full outline-none'/>
             <button className='bg-blue-500 rounded-md px-3 mr-3'>Search</button>
           </form>
         </div>
